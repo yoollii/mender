adda1e37215358b22bebd55dc4a8551dc039401f
yoollii <yoollii@github.com>
2018-07-23 16:39:12 +0800

    上传错误

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,63 +1,43 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
-import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
-import { ButtonsComponent } from './home/buttons/buttons.component';
-import { TableComponent } from './home/table/table.component';
-import { FormComponent } from './home/form/form.component';
-import { CardsComponent } from './home/cards/cards.component';
-import { SimpleTableComponent } from './home/simple-table/simple-table.component';
-import { UploadComponent } from './home/upload/upload.component';
-import { PanelComponent } from './home/panel/panel.component';
-import { CarouselComponent } from './home/carousel/carousel.component';
-import { StepsComponent } from './home/steps/steps.component';
-import { ProgressComponent } from './home/progress/progress.component';
-import { ProgressprogressComponent } from './home/progressprogress/progressprogress.component';
-import { MentionComponent } from './home/mention/mention.component';
-import { AntvComponent } from './home/antv/antv.component';
+import { OrderPage } from '../pages/order/order';
+import { OrdermapPage } from '../pages/ordermap/ordermap';
+import { MePage } from '../pages/me/me';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
 
-registerLocaleData(zh);
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    ButtonsComponent,
-    TableComponent,
-    FormComponent,
-    CardsComponent,
-    SimpleTableComponent,
-    UploadComponent,
-    PanelComponent,
-    CarouselComponent,
-    StepsComponent,
-    ProgressComponent,
-    ProgressprogressComponent,
-    MentionComponent,
-    AntvComponent
+    MyApp,
+    MePage,
+    OrdermapPage,
+    OrderPage,
+    HomePage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    RouterModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    NgZorroAntdModule,
-    AppRoutingModule,
+    IonicModule.forRoot(MyApp)
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
-  bootstrap: [AppComponent]
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MePage,
+    OrdermapPage,
+    OrderPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  nowTime:string;
+  nowDay:string;
   constructor(public navCtrl: NavController) {
 
+  }
+  ionViewDidLoad() {
+  	let nowtime=new Date();
+  	this.nowTime=nowtime.getHours()+' : '+nowtime.getMinutes();
+  	let da=nowtime.getDate(); //获取当前日 
+    let mon=nowtime.getMonth()+1; //获取当前月份 
+    let day=nowtime.getDay(); //获取当前星期几  
+  	this.nowDay=mon+'月'+da+'日   星期'+day;
+  	setTimeout(function(){
+  		this.nowTime=nowtime.getHours()+' : '+nowtime.getMinutes();
+  	},60000);
   }
 
 }
