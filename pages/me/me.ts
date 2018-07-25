b5efbc8c88e783f48dbdb8c0dd40d7dba89fbf4f
yoollii <yoollii@github.com>
2018-07-25 09:48:44 +0800

    修改me

--- pages/me/me.ts
+++ pages/me/me.ts
@@ -14,8 +14,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'me.html',
 })
 export class MePage {
-
+  private Lists=[];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.Lists=[{title:"消息通知",src:"../../assets/imgs/person/msg.png"},
+    {title:"我的配件",src:"../../assets/imgs/person/parts.png"},
+    {title:"个人信息",src:"../../assets/imgs/person/person-msg.png"},
+    {title:"绩效管理",src:"../../assets/imgs/person/performance.png"},
+    {title:"师徒管理",src:"../../assets/imgs/person/teachers.png"},
+    {title:"推广管理",src:"../../assets/imgs/person/extension.png"}];
   }
 
   ionViewDidLoad() {
