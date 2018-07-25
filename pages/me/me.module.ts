629eab738fa464bb1f3b28c559a39fa9bf2d9607
wuzaoyonggg <791173473@qq.com>
2018-07-25 09:53:53 +0800

    提交module

--- pages/me/me.module.ts
+++ pages/me/me.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MePage } from './me';
+import {PeopleInformationPage} from "../people-information/people-information";
+
 
 @NgModule({
   declarations: [
-    MePage,
+    MePage
   ],
   imports: [
     IonicPageModule.forChild(MePage),
-  ],
+  ]
 })
 export class MePageModule {}
