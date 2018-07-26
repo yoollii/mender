9c956313c67b8b884f0537e484e07d7f20fc70d1
wuzaoyonggg <791173473@qq.com>
2018-07-26 18:19:56 +0800

    更新个人信息/2018-7-26 18:19

--- pages/me-people-information/tel-update/tel-update.ts
+++ pages/me-people-information/tel-update/tel-update.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the TelUpdatePage page.
@@ -15,11 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TelUpdatePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  isHidden = true;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertController: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TelUpdatePage');
   }
 
+  telSure() {
+    this.isHidden = false;
+  }
 }
