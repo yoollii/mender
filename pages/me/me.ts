1cf92588c1ddc413d5638e8a15a7217d8f96d905
yoollii <yoollii@github.com>
2018-07-30 14:55:36 +0800

    配件管理

--- pages/me/me.ts
+++ pages/me/me.ts
@@ -5,6 +5,7 @@ import { MessageNoticePage } from '../me-message-notice/message-notice';
 import { PerformancePage } from '../me-performance/performance';
 import { TeachersPage } from './me-teachers/teachers';
 import { TwocodePage } from './twocode/twocode';
+import { PartsMallPage } from '../parts-mall/parts-mall';
 
 /**
  * Generated class for the MePage page.
@@ -25,7 +26,7 @@ export class MePage {
   };
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.Lists=[{title:"消息通知",src:"../../assets/imgs/person/msg.png",nav:MessageNoticePage,classIcon:"notice-icon"},
-    {title:"我的配件",src:"../../assets/imgs/person/parts.png",nav:"",classIcon:"parts-icon"},
+    {title:"我的配件",src:"../../assets/imgs/person/parts.png",nav:PartsMallPage,classIcon:"parts-icon"},
     {title:"个人信息",src:"../../assets/imgs/person/person-msg.png",nav:PeopleInformationPage,classIcon:"persion-icon"},
     {title:"绩效管理",src:"../../assets/imgs/person/performance.png",nav:PerformancePage,classIcon:"performance-icon"},
     {title:"师徒管理",src:"../../assets/imgs/person/teachers.png",nav:TeachersPage,classIcon:"teachers-icon"}]
@@ -38,5 +39,5 @@ export class MePage {
   qrcode(){
   	this.navCtrl.push(TwocodePage);
   }
-  
+
 }
