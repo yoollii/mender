42e6b07a15a91ca0eba7f43377b4d142fce9c98e
wuzaoyonggg <791173473@qq.com>
2018-07-31 17:35:35 +0800

    新增配件流水页面

--- pages/me/parts-stream/parts-stream.ts
+++ pages/me/parts-stream/parts-stream.ts
@@ -15,7 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PartsStreamPage {
 
+  parts = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.parts = [
+      {orderCode: '201823435312',img: '../../assets/imgs/licImg-ys.png',model: 'iphone6',fault: '电池故障-电池膨胀/续航时间短',color: '银色',solve: '使用S内屏幕*1 S8触摸键*1'},
+      {orderCode: '201823435312',img: '../../assets/imgs/licImg-ys.png',model: 'iphone6',fault: '电池故障-电池膨胀/续航时间短',color: '银色',solve: '使用S内屏幕*1 S8触摸键*1'},
+      {orderCode: '201823435312',img: '../../assets/imgs/licImg-ys.png',model: 'iphone6',fault: '电池故障-电池膨胀/续航时间短',color: '银色',solve: '使用S内屏幕*1 S8触摸键*1'},
+      {orderCode: '201823435312',img: '../../assets/imgs/licImg-ys.png',model: 'iphone6',fault: '电池故障-电池膨胀/续航时间短',color: '银色',solve: '使用S内屏幕*1 S8触摸键*1'},
+      {orderCode: '201823435312',img: '../../assets/imgs/licImg-ys.png',model: 'iphone6',fault: '电池故障-电池膨胀/续航时间短',color: '银色',solve: '使用S内屏幕*1 S8触摸键*1'}
+    ];
   }
 
   ionViewDidLoad() {
