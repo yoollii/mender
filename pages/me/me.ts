bcbff52b483b14e816a49abae01c05717f4dcf5c
wuzaoyonggg <791173473@qq.com>
2018-08-01 11:38:12 +0800

    更新我的配件导航/2018-8-1 11:38

--- pages/me/me.ts
+++ pages/me/me.ts
@@ -4,6 +4,7 @@ import { PeopleInformationPage } from '../me-people-information/people-informati
 import { MessageNoticePage } from '../me-message-notice/message-notice';
 import { PerformancePage } from '../me-performance/performance';
 import { TeachersPage } from './me-teachers/teachers';
+import {MyPartsPage} from "./my-parts/my-parts";
 
 /**
  * Generated class for the MePage page.
@@ -24,7 +25,7 @@ export class MePage {
   };
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.Lists=[{title:"消息通知",src:"../../assets/imgs/person/msg.png",nav:MessageNoticePage,classIcon:"notice-icon"},
-    {title:"我的配件",src:"../../assets/imgs/person/parts.png",nav:"",classIcon:"parts-icon"},
+    {title:"我的配件",src:"../../assets/imgs/person/parts.png",nav:MyPartsPage,classIcon:"parts-icon"},
     {title:"个人信息",src:"../../assets/imgs/person/person-msg.png",nav:PeopleInformationPage,classIcon:"persion-icon"},
     {title:"绩效管理",src:"../../assets/imgs/person/performance.png",nav:PerformancePage,classIcon:"performance-icon"},
     {title:"师徒管理",src:"../../assets/imgs/person/teachers.png",nav:TeachersPage,classIcon:"teachers-icon"}]
