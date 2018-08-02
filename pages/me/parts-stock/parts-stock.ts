b9f54c2959f1944ffe6a414e60c379c80ffa7549
wuzaoyonggg <791173473@qq.com>
2018-08-02 15:05:01 +0800

    更新配件库存

--- pages/me/parts-stock/parts-stock.ts
+++ pages/me/parts-stock/parts-stock.ts
@@ -14,12 +14,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'parts-stock.html',
 })
 export class PartsStockPage {
-
+  navList = ['屏幕','按键', '外框', '喇叭', '相机', '电池', '主板', '其他'];
+  navIndex =0;
+  products = [
+    {img: 'assets/imgs/person/notice/phone.jpg', name: 'iphone6 内屏', price: '499', num: '0'},
+    {img: 'assets/imgs/person/notice/phone.jpg', name: 'iphone7 内屏', price: '599', num: '1'},
+    {img: 'assets/imgs/person/notice/phone.jpg', name: 'iphone8 内屏', price: '699', num: '2'},
+    {img: 'assets/imgs/person/notice/phone.jpg', name: 'iphone9 内屏', price: '799', num: '0'},
+    {img: 'assets/imgs/person/notice/phone.jpg', name: 'iphone10 内屏', price: '899', num: '3'},
+    {img: 'assets/imgs/person/notice/phone.jpg', name: 'iphone11 内屏', price: '999', num: '1'},
+    {img: 'assets/imgs/person/notice/phone.jpg', name: 'iphone12 内屏', price: '1099', num: '2'},
+    {img: 'assets/imgs/person/notice/phone.jpg', name: 'iphone13 内屏', price: '1199', num: '3'},
+  ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PartsStockPage');
   }
+  navChange(index){
+    if(this.navIndex!=index){
+      this.navIndex = index;
+    }
 
+  }
 }
