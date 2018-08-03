724eeaf62f089aa5a9e1c6fd3754c300093a54c4
wuzaoyonggg <791173473@qq.com>
2018-08-03 14:57:34 +0800

    更新申请配件

--- pages/me/apply-parts-two/apply-parts-two.ts
+++ pages/me/apply-parts-two/apply-parts-two.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HttpServiceProvider} from "../../../providers/http-service/http-service";
 
 /**
  * Generated class for the ApplyPartsTwoPage page.
@@ -14,12 +15,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'apply-parts-two.html',
 })
 export class ApplyPartsTwoPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  buyNumber = 0;
+  navList = [];
+  navIndex =0;
+  products = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpServiceProvider) {
+    this.http.request({
+      url: 'my/applypartsmenu',
+      type: 'get',
+      success: res => res.reduce((str, nav) => this.navList.push(nav.title), '')
+    });
+    this.http.request({
+      url: 'my/applypartsdetail',
+      data : {classifyid: 1, current: 1, size: 10},
+      success: res => this.products = res.records
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ApplyPartsTwoPage');
   }
-
+  navChange(index) {
+    if (this.navIndex != index) {
+      this.navIndex = index;
+    }
+    this.http.request({
+      url: 'my/applypartsdetail',
+      data : {classifyid: index + 1, current: 1, size: 10},
+      success: res => this.products = res.records
+    });
+  }
 }
