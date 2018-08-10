08957eaf733b10ab2197c7606b1a4eff7305f53b
wuzaoyonggg <791173473@qq.com>
2018-08-10 16:19:58 +0800

    更新所有学生

--- pages/me/me-all-students/me-all-students.ts
+++ pages/me/me-all-students/me-all-students.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { HttpServiceProvider } from '../../../providers/http-service/http-service';
 import { PageDataProvider } from '../../../providers/page-data/page-data';
 import { StorageServiceProvider } from '../../../providers/storage-service/storage-service';
@@ -22,30 +22,59 @@ export class MeAllStudentsPage {
 
   apprentices = [];
   haveData = true;
+  title: string;
+  isHidden = true;
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private http: HttpServiceProvider,
               private pageData: PageDataProvider,
-              private storage: StorageServiceProvider) {
+              private storage: StorageServiceProvider,
+              private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
-    this.getAllApprentice();
+    this.title = this.navParams.get('title');
+    console.log(this.title);
+    if(this.title == '所有徒弟') {
+      this.isHidden = true;
+      this.getAllApprentice();
+    }
+    if(this.title == '申请列表') {
+      this.isHidden = false;
+      this.getwantmyfollower();
+    }
   }
-  getAllApprentice(operation?: any) {
-    let flag = operation?false:true;
+  getAllApprentice() {
     this.http.request({
       url: 'my/masterapprenticelistall',
       type: 'get',
-      success: res => this.apprentices = res,
-      complete: res => {
-        if(operation) {
-          operation.complete();
-        }
-      }
+      success: res => this.apprentices = res
+    });
+  }
+  getwantmyfollower() {
+    this.http.request({
+      url: 'my/getwantmyfollower',
+      type: 'get',
+      success: res => this.apprentices = res
     });
   }
   goToStudent(student: Object) {
     this.storage.write('student', student);
     this.navCtrl.push(PerformancePage);
+  }
+  sureGetApprentice(worknum: string) {
+    this.http.request({
+      url: 'my/entermyfollower/'+worknum,
+      type: 'get',
+      success: res => {
+        this.tipsAlert('收徒成功')
+      }
+    });
+  }
+  tipsAlert(str: string) {
+    const alert = this.alertCtrl.create({
+      cssClass: 'tips-alert',
+      title: str
+    })
+    alert.present();
   }
 }
